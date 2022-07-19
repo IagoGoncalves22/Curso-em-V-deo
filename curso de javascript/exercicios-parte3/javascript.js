dd7fe8c14f524ec82999118c0532793aca7b21f0
Iago Goncalves <iago.g.goncalves@gmail.com>
2022-07-18 22:46:54 -0300

    exercicio-verificaridade-concluído

--- curso de javascript/exercicios-parte3/javascript.js
+++ curso de javascript/exercicios-parte3/javascript.js
@@ -9,12 +9,44 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             gênero = 'homem'
+            if (idade >= 0 && idade < 10) {
+                // criança
+                img.setAttribute('src', 'homemcriança.jpg')
+            } else if (idade < 21) {
+                // jovem
+                img.setAttribute('src', 'homemadolescente.jpg')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'homemadulto.jpg')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'homemidoso.jpg')
+            }
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                // criança
+                img.setAttribute('src', 'mulhercriança.jpg')
+            } else if (idade < 21) {
+                // jovem
+                img.setAttribute('src', 'mulheradolescente.jpg')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'mulheradulto.jpg')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'mulheridosa.jpg')
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+        res.appendChild(img)
+
+
+
     }
 }
